6c6f93a99f68e7f5f348bfd540e106c74763165a
MaruquitaR <52496526+MaruquitaR@users.noreply.github.com>
2019-07-03 07:48:38 -0500

    Initial release
    Es solo un esquema de la aplicación en si

---
+++ kfc/js/loader.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#miuhqui1").click(function(event) {
+        $("#capa").load('index.html');
+    });
+});
+
+$(document).ready(function() {
+    $("#miuhqui2").click(function(event) {
+        $("#capa").load('dir/registry.html');
+    });
+});
